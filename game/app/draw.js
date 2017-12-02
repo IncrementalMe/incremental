8803a2c84064cb87673a1593550943b40e8ec406
IncrementalMe <incrementalme@gmail.com>
2017-12-02 23:24:47 +1300

    Fix canvas scaling badly, add sample text

--- game/app/draw.js
+++ game/app/draw.js
@@ -1,6 +1,18 @@
 define(function (require) {
-  return function draw (ctx) {
-    ctx.fillStyle = '#f7f7f7'
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  return {
+    centerText: function (ctx, text, y, xOffset = 0) {
+      var measurement = ctx.measureText(text)
+      var x = (ctx.canvas.width - measurement.width) / 2 + xOffset
+      ctx.fillText(text, x, y)
+    },
+
+    draw: function (ctx) {
+      var y = ctx.canvas.height / 2
+      var color = 'rgb(0,0,0)'
+
+      ctx.fillStyle = color
+      ctx.font = '24px monospace'
+      this.centerText(ctx, 'Test text', y - 30)
+    }
   }
 })
--- game/app/start.js
+++ game/app/start.js
@@ -2,8 +2,12 @@ define(function (require) {
   var draw = require('./draw.js')
   var ctx = document.getElementById('canvas').getContext('2d')
 
+  ctx.canvas.setAttribute('width', 900)
+  ctx.canvas.setAttribute('height', 650)
+
   function update () {
-    draw(ctx)
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    draw.draw(ctx)
     window.requestAnimationFrame(update)
   }
 
