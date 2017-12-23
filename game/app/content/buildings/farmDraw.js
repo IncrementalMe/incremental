68fbdd27a879b08b7c3a0f8ed8e84655b810cd29
IncrementalMe <incrementalme@gmail.com>
2017-12-23 20:53:24 +1300

    Change larger draw functions to use ctx.translate for easier repositioning

--- game/app/content/buildings/farmDraw.js
+++ game/app/content/buildings/farmDraw.js
@@ -15,15 +15,15 @@ define(function (require) {
     ctxUtils.wrap(ctx, drawParts.buttons)(game, ctx)
     ctxUtils.wrap(ctx, sfx.draw)(ctx)
 
-    if (farm.built) {
+    if (farm.built === false) {
+      ctxUtils.wrap(ctx, drawParts.build)(game, ctx)
+    } else {
       ctxUtils.wrap(ctx, drawParts.normal)(ctx, farm)
 
       if (logicUpdates >= settings.ups) {
         sfx.createSprite(ctx.images.food, { x: 444, y: 546 })
         logicUpdates -= settings.ups
       }
-    } else {
-      ctxUtils.wrap(ctx, drawParts.build)(game, ctx)
     }
   }
 
@@ -45,38 +45,42 @@ define(function (require) {
       }
       ctx.drawImage(ctx.images.farm, 450 - 39, 513, 76, 76)
     },
+
     build: function (game, ctx) {
       var farm = game.buildings.farm
       var btt = farm.buttons.get('click')
+      ctx.translate(btt.x - btt.width / 2, btt.y - btt.height / 2)
       ctx.font = '22px monospace'
 
       if (btt.hover && farm.canBuild(game)) {
         ctx.fillStyle = '#2a2'
         btt.fill = '#2a2'
 
-        var x = btt.x - btt.width / 2
-        var y = btt.y - btt.height / 2
         ctx.globalAlpha = 0.1
-        ctx.fillRect(x, y, btt.width, btt.height)
+        ctx.fillRect(0, 0, btt.width, btt.height)
         ctx.globalAlpha = 1
-        ctxUtils.wrap(ctx, sfx.happyDraw)(ctx, 'Build Farm', 389, 502, count)
+        sfx.happyDraw(ctx, 'Build Farm', -1, -4, count)
       } else {
-        ctx.fillText('Build Farm', 389, 502)
+        ctx.fillText('Build Farm', -1, -4)
       }
 
       ctx.font = '20px monospace'
       ctx.textAlign = 'right'
       var cost = formatNumber(farm.buildCost.get('food'))
-      ctx.fillText(cost, 450, 602)
-      ctx.drawImage(ctx.images.food, 459, 586, 18, 18)
+      ctx.fillText(cost, 60, 96)
+      ctx.drawImage(ctx.images.food, 69, 80, 18, 18)
     },
+
     normal: function (ctx, farm) {
+      var btt = farm.buttons.get('click')
+      ctx.translate(btt.x - btt.width / 2, btt.y - btt.height / 2)
+
       ctx.font = '22px monospace'
-      ctx.fillText('Farm', 389, 502)
+      ctx.fillText('Farm', -1, -4)
 
       if (farm.points > 0) {
         ctx.textAlign = 'right'
-        ctx.fillText('+' + farm.points, 511, 502)
+        ctx.fillText('+' + farm.points, 21, -4)
       }
 
       ctx.font = '20px monospace'
@@ -84,10 +88,11 @@ define(function (require) {
       if (text > 0) text = '+' + text
       else text = '-' + text
 
-      ctx.fillText(text, 420, 603)
-      ctx.fillText('/s', 428 + ctx.measureText(text).width, 603)
-      ctx.drawImage(ctx.images.food, 393, 585, 21, 21)
+      ctx.fillText(text, 30, 97)
+      ctx.fillText('/s', 38 + ctx.measureText(text).width, 97)
+      ctx.drawImage(ctx.images.food, 3, 79, 21, 21)
     },
+
     buttons: function (game, ctx) {
       game.buildings.farm.buttons.forEach(btt => {
         btt.draw(game, ctx)
